896a510aa03966cc4bf54f649304c092510b14f0
Rafael Fernando Rutsatz <rafael.rutsatz@compasso.com.br>
2020-03-30 13:36:15 -0300

    Refatorado consumidores.

--- src/main/java/br/com/alura/ecommerce/EmailService.java
+++ src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -1,50 +1,31 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class EmailService {
     public static void main(String[] args) throws InterruptedException {
-
-        var consumer = new KafkaConsumer<String, String>(properties());
-        // Me inscrevo como um escutador. Eu passo uma lista por parâmetro, com o nome dos tópicos
-        // que quero me inscrever. Apesar de passarmos uma lista, nos inscrevemos em somente um tópico,
-        // pois se não, fica muito bagunçado.
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
-        while (true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            if (!records.isEmpty()) {
-                System.out.println("Encontrei " + records.count() + " registros");
-                for (var record : records) {
-                    System.out.println("------------------------------------");
-                    System.out.println("Send email");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    Thread.sleep(1000);
-                }
-                System.out.println("Email sent");
-            }
-        }
+        var emailService = new EmailService();
+        var service = new KafkaService(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL",
+                emailService::parse);
+        service.run();
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        // Da mesma forma como configuramos os serializadores, temos que configurar os desserializadores.
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        // Eu também preciso dizer o grupo que o consumidor pertence, para pode controlar e garantir
-        // que ele receba todas as mensagens. Nesse caso, usei o próprio nome da classe como o ID do grupo.
-        // Se eu tenho somente um serviço com esse grupo, eu recebo todas as mensagens. Se tiver mais alguém
-        // escutando com o mesmo grupo, as mensagens são distribuidas entre os dois serviços.
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-        return properties;
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------");
+        System.out.println("Send email");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Email sent");
     }
 }
+
+
+
--- src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -1,4 +1,60 @@
 package br.com.alura.ecommerce;
 
-public class KafkaService {
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
+class KafkaService {
+    private final KafkaConsumer<String, String> consumer;
+    private final ConsumerFunction parse;
+
+    KafkaService(String groupId, String topic, ConsumerFunction parse) {
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<String, String>(properties(groupId));
+
+        // Me inscrevo como um escutador. Eu passo uma lista por parâmetro, com o nome dos tópicos
+        // que quero me inscrever. Apesar de passarmos uma lista, nos inscrevemos em somente um tópico,
+        // pois se não, fica muito bagunçado.
+        this.consumer.subscribe(Collections.singletonList(topic));
+
+    }
+
+    void run() {
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()) {
+                System.out.println("Encontrei " + records.count() + " registros");
+                for (var record : records) {
+                    parse.consume(record);
+                }
+            }
+        }
+    }
+
+    private static Properties properties(String groupId) {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        // Da mesma forma como configuramos os serializadores, temos que configurar os desserializadores.
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // Eu também preciso dizer o grupo que o consumidor pertence, para pode controlar e garantir
+        // que ele receba todas as mensagens. Nesse caso, usei o próprio nome da classe como o ID do grupo.
+        // Se eu tenho somente um serviço com esse grupo, eu recebo todas as mensagens. Se tiver mais alguém
+        // escutando com o mesmo grupo, as mensagens são distribuidas entre os dois serviços.
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        // Posso configurar o nome para o consumer. Mas preciso garantir que esse nome seja único, e não se
+        // repita entre as demais instâncias.
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+        // Posso configurar quantas mensagens ele vai pegar em cada poll. Como quero diminuir o tempo de commit,
+        // coloco para pegar uma mensagem por poll, pois assim a cada mensagem ele faz um commit, e evita
+        // o problema de tentar fazer um commit quando o kafka fez um rebalanceamento. Essa é uma configuração
+        // bastante utilizada, inclusive por empresas grandes.
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+        return properties;
+    }
 }
